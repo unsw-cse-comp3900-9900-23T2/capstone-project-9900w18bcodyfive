da4e9e61ae279b1d4cbd53916f3c630869f09ee4
Sevugan Chetty Kathiresan <sevugankathiresan@gmail.com>
2023-06-24 15:56:07 +1000

    Home Page Completed

--- kellner/manager-ui/src/components/HomeHeader.jsx
+++ kellner/manager-ui/src/components/HomeHeader.jsx
@@ -1,22 +1,51 @@
 import styled from '@emotion/styled';
 import logo from '../assets/logo/Kellner-Logo.png';
+import Button from '@mui/material/Button';
+
+const MasterContainer = styled('div')({
+    display: 'flex',
+    justifyContent: 'space-between'
+});
 
 const Container = styled('div')({
+    margin: 0,
+    padding: 0,
     display: 'flex',
-    maxHeight: '20vh',
-    background: 'blue'
+    maxHeight: '15vh',
+    background: 'none',
 });
 
 const LogoContainer = styled('img')({
-    width: '20vw',
+    width: '15vw',
     objectFit: 'contain',
 });
 
+const NameContainer = styled('div')({
+    margin: 0,
+    padding: '2rem 0rem',
+    fontWeight: 'bolder',
+    fontSize: '4rem',
+});
+
+const ButtonContainer = styled('div')({
+    display: 'flex',
+    alignItems: 'center',
+    minWidth: '20vw',
+    justifyContent: 'space-evenly'
+})
+
 const HomeHeader = () => {
     return(
-        <Container>
-            <LogoContainer src={logo}/>
-        </Container>
+        <MasterContainer>
+            <Container>
+                <LogoContainer src={logo}/>
+                <NameContainer><span style={{color:'#006600'}}>Kell</span>ner</NameContainer>
+            </Container>
+            <ButtonContainer>
+                <Button variant="contained" color='success' size='large'>Log In</Button>
+                <Button variant="contained" color='success' size='large'>Sign Up</Button>
+            </ButtonContainer>
+        </MasterContainer>
     );
 }
 
--- kellner/manager-ui/src/pages/Home.jsx
+++ kellner/manager-ui/src/pages/Home.jsx
@@ -2,19 +2,46 @@ import React from 'react';
 import { styled } from '@mui/material';
 import HomeBg from '../assets/images/Home-Bg.jpg';
 import HomeHeader from '../components/HomeHeader';
+import Slide from '@mui/material/Slide';
 
 const Container = styled('div')({
+    margin: 0,
+    padding: 0,
     minHeight: '100vh',
     backgroundImage: `url(${HomeBg})`,
     backgroundRepeat: 'no-repeat',
     backgroundSize: 'cover',
-    opacity: 0.75
+    opacity: 0.75,
+    fontFamily: 'Nunito',
+});
+
+const WelcomeMessageContainer = styled('div')({
+    marginTop: '12rem',
+    marginLeft: '5rem',
+    fontFamily: 'Nunito',
+    fontSize: '3rem',
+    fontWeight: 'bold',
+    textAlign: 'left',
 });
 
 const Home = () => {
+    const [checked, setChecked] = React.useState(false);
+
+    // Triggers the animation everytime the page loads
+    React.useEffect(() => {
+        setChecked((checked) => !checked);
+    },[]);
+
     return(
         <Container>
             <HomeHeader />
+            <Slide  direction="up" in={checked} {...(checked ? {timeout: 1000} : {}) }collapsedHeight = {50}>
+                <WelcomeMessageContainer>
+                    <span style={{ fontSize:"5rem"}}>Welcome to <span style={{color:'#006600'}}>Kell</span>ner.</span><br/>
+                    Your One stop platform to power your Restaurant Operations.
+                </WelcomeMessageContainer>
+            </Slide>
+            
         </Container>
     );
 }
