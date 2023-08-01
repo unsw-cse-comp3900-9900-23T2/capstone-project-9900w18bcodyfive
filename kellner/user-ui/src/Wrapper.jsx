361e49cdbd1278eb3d2f95ab92f30957eb17b22e
Sevugan Chetty Kathiresan <sevugankathiresan@gmail.com>
2023-08-02 00:44:14 +1000

    thanks page completed in user ui

--- kellner/user-ui/src/Wrapper.jsx
+++ kellner/user-ui/src/Wrapper.jsx
@@ -11,6 +11,7 @@ import OrderPlaced from "./pages/OrderPlaced";
 import OrderPrepared from "./pages/OrderPrepared";
 import EnjoyYourMeal from "./pages/EnjoyYourMeal";
 import CheckoutPage from "./pages/CheckoutPage";
+import ThankYou from "./pages/thankYou";
 
 
 const Wrapper = ()=>{
@@ -25,6 +26,7 @@ const Wrapper = ()=>{
             <Route path="/order-prepared" element={<OrderPrepared/>}/>
             <Route path="/enjoy-your-meal" element={<EnjoyYourMeal/>}/>
             <Route path="/checkout" element={<CheckoutPage/>}/>
+            <Route path="/thanks-page" element={<ThankYou/>}/>
         </Routes>
     );
 };
---
+++ kellner/user-ui/src/pages/thankYou.jsx
@@ -0,0 +1,48 @@
+import React from "react";
+import styled from "@emotion/styled";
+import { useNavigate } from "react-router-dom";
+
+// component imports
+import thanks from "../assets/images/thanks.gif";
+import PlainHeader from "../components/PlainHeader";
+
+//redux imports
+import { useDispatch } from "react-redux";
+import { clearCart } from "../redux/slices/cartSlice";
+import { removeOrderNo } from "../redux/slices/orderSlice";
+import { removeTable } from "../redux/slices/tableSlice";
+import { removeRestaurant } from "../redux/slices/restaurantSlice";
+
+const Container = styled('div')({
+    display: 'flex',
+    justifyContent: 'center',
+    fontFamily: 'Nunito',
+    fontSize: '2rem',
+    fontWeight: 'bold',
+    marginTop: '9rem'
+});
+const ThankYou = () => {
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
+    const clearEverything = ()=>{
+        dispatch(clearCart());
+        dispatch(removeOrderNo());
+        dispatch(removeTable());
+        dispatch(removeRestaurant());
+        navigate('/');
+    }
+
+    React.useEffect(()=>{
+        window.setTimeout(clearEverything, 5000);
+    }, []);
+    return(
+        <>
+            <PlainHeader/>
+            <Container>Thanks for visiting us</Container>
+            <img src={thanks} alt={'thank you'} style={{height:'500px', borderRadius:"2rem", margin:'2rem'}}/>
+        </>
+    );
+
+}
+
+export default ThankYou;
